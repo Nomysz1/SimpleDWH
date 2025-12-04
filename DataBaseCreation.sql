a9c162e339e3a3e4203594b7009b85aed846d016
Simon <simonarch12@gmail.com>
2025-12-05 00:45:59 +0100

    I simplified the SQL code and decided to add a creation date to the previously created database

--- DataBaseCreation.sql
+++ DataBaseCreation.sql
@@ -1,10 +1,10 @@
-DECLARE @DataBaseName nvarchar(48) = 'DWH'
+DECLARE @DataBaseName nvarchar(48) = 'DataWarehouse'
 DECLARE @SQLCreationCode nvarchar(MAX) 
 
 IF DB_ID(@DataBaseName) IS NULL
 BEGIN
 	BEGIN TRY
-		SET @SQLCreationCode = 'CREATE DATABASE ' + @DataBaseName + CAST(CAST(GETDATE() as int) as varchar)
+		SET @SQLCreationCode = 'CREATE DATABASE ' + @DataBaseName
 		exec sp_executesql @SQLCreationCode
 	END TRY
 	BEGIN CATCH
@@ -12,4 +12,7 @@ BEGIN
 	END CATCH
 END 
 ELSE
-	PRINT 'The database has already been created.'
+BEGIN
+	DECLARE @CreationDate as datetime = (SELECT create_date FROM sys.databases WHERE database_id=DB_ID(@DataBaseName))
+	PRINT 'The database has already been created at:' + CAST(@CreationDate as varchar)
+END
